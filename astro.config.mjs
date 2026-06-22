8f4208e92f942a92109317356732dbf63f9f0b38
DRD161 <dylan@dylandavenport.com>
2026-06-21 23:45:24 -0400

    Setup astro.config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,77 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: 'SpaceMono',
+      cssVariable: '--font-space-mono',
+      options: {
+        variants: [
+          {
+            src: ['./src/assets/fonts/SpaceMono-Regular.ttf'],
+            weight: 'normal',
+            style: 'normal',
+          },
+        ],
+      },
+    },
+    {
+      provider: fontProviders.local(),
+      name: 'BarlowCondensed',
+      cssVariable: '--font-barlow-condensed',
+      options: {
+        variants: [
+          {
+            src: ['./src/assets/fonts/BarlowCondensed-Regular.ttf'],
+            weight: '400',
+            style: 'normal',
+          },
+          {
+            src: ['./src/assets/fonts/BarlowCondensed-Black.ttf'],
+            weight: '900',
+            style: 'normal',
+          },
+          {
+            src: ['./src/assets/fonts/BarlowCondensed-Bold.ttf'],
+            weight: '700',
+            style: 'normal',
+          },
+          {
+            src: ['./src/assets/fonts/BarlowCondensed-SemiBold.ttf'],
+            weight: '600',
+            style: 'normal',
+          },
+        ],
+      },
+    },
+    {
+      provider: fontProviders.local(),
+      name: 'Barlow',
+      cssVariable: '--font-barlow',
+      options: {
+        variants: [
+          {
+            src: ['./src/assets/fonts/Barlow-Light.ttf'],
+            weight: '300',
+            style: 'normal',
+          },
+          {
+            src: ['./src/assets/fonts/Barlow-Regular.ttf'],
+            weight: '400',
+            style: 'normal',
+          },
+          {
+            src: ['./src/assets/fonts/Barlow-Medium.ttf'],
+            weight: '500',
+            style: 'normal',
+          },
+        ],
+      },
+    },
+  ],
+});
